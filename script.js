d7a70e8984920f161b361400ce35b384c0098ca4
@mohideenkaja377 <mohideenkaja377@gmail.com>
2024-07-03 22:48:53 +0530

    updateD

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 async function myFunction() {
     window.open('file/MohideenKajaResume-1.pdf')
 }
+
+$(function () {
+    
+    $("#header").load("header.html");
+    // $("#footer").load("footer.html");
+});
